a1462860b4cb1230243af69194c892934b5ddb1f
mhqb365 <mhqb365@gmail.com>
2022-03-29 00:19:24 +0700

    :rocket:

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,7 +2,10 @@
   <CFooter>
     <div>
       <a href="https://webdicebot.xyz" target="_blank">WebDiceBot.Xyz</a>
-      <span class="ms-1">&copy; {{ new Date().getFullYear() }}</span>
+      <span class="ms-1">
+        &copy; {{ new Date().getFullYear() }} by
+        <a href="https://mhqb365.com" target="_blank">mhqb365.com</a>
+      </span>
     </div>
   </CFooter>
 </template>
